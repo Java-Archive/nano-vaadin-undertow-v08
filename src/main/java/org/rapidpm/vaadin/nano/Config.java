77f4690aa86a3b114f94d4383a9b48c7aa1c781d
Sven Ruppert <sven.ruppert@gmail.com>
2018-07-25 09:58:57 +0200

    added lic header

--- src/main/java/org/rapidpm/vaadin/nano/Config.java
+++ src/main/java/org/rapidpm/vaadin/nano/Config.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2017 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.rapidpm.vaadin.nano;
 
 import org.rapidpm.frp.model.serial.Triple;
@@ -9,9 +24,15 @@ public class Config extends Triple<String, Integer, Class<? extends ComponentSup
     super(host, port, supplierClass);
   }
 
-  public String host() { return getT1();}
+  public String host() {
+    return getT1();
+  }
 
-  public Integer port() { return getT2();}
+  public Integer port() {
+    return getT2();
+  }
 
-  public Class<? extends ComponentSupplier> supplierClass() { return getT3();}
+  public Class<? extends ComponentSupplier> supplierClass() {
+    return getT3();
+  }
 }
